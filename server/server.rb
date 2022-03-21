a2d851eecc420cec7cb1409568c36a5a9cff3e16
Immanuel Currah <immanueli.currah@gmail.com>
2022-03-21 12:02:48 -0400

    creating payment intent

--- server/server.rb
+++ server/server.rb
@@ -26,11 +26,27 @@ end
 #   data.to_json
 # end
 
+def calculate_order_amount(_items)
+  # Replace this constant with a calculation of the order's amount
+  # Calculate the order total on the server to prevent
+  # people from directly manipulating the amount on the client
+  1400
+end
+
 post "/create-payment-intent" do 
   content_type "application/json"
   data = JSON.parse(request.body.read)
 
-  
+  payment_intent = Stripe::PaymentIntent.create(
+    amount: calculate_order_amount(data["items"])
+    currency: "eur"
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  )
+  {
+    clientSecret: payment_intent["client_secret"]
+  }.to_json
 end
 
 post '/webhook' do
